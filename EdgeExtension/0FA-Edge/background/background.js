d1530b61ef9889fe7909dcc6ef56512814e351f6
Adrian Kane <egg_on_stilts@hotmail.com>
2024-07-11 18:46:06 +1000

    more tinkering

---
+++ EdgeExtension/0FA-Edge/background/background.js
@@ -0,0 +1,11 @@
+// By GPT
+/*
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'login') {
+      chrome.tabs.executeScript({
+        file: 'content_script.js'
+      });
+    }
+  });
+
+  */
--- EdgeExtension/0FA-Edge/popup/popup.js
+++ EdgeExtension/0FA-Edge/popup/popup.js
@@ -2,9 +2,10 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const loginButton = document.getElementById('loginButton');
-  
+    console.log('clickclick ready'); //debug
     loginButton.addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        console.log('clickclick'); //debug
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'login' }, function(response) {
           console.log('Login message sent');
         });
